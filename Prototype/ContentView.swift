23b18c0f8f7869a17e43139033f34c6e97f1d8ca
Abdul Wahab <abdul.wahab@danatonline.com>
2020-07-19 00:07:12 +0500

    Initial Commit

---
+++ Prototype/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Prototype
+//
+//  Created by Abdul Wahab on 7/19/20.
+//  Copyright © 2020 Abdul Wahab. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
